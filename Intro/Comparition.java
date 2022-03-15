5ccf0c097692f356fb58abe304afe51e6ff7b52d
sathis <latchulatchu02@gmail.com>
2022-03-15 11:41:12 +0530

    first commit

---
+++ Intro/Comparition.java
@@ -0,0 +1,28 @@
+class Comparition{
+    public static void main(String[] args){
+        int a=25;
+        int b=20;
+        if(a>b) {
+            System.out.println("a is graterthen b");
+        }
+            if(a<b) {
+                System.out.println("a is lessthen b");
+            }
+                if(a>=b) {
+                    System.out.println("a greterthen or equal to b");
+                }
+                    if(a<=b) {
+                        System.out.println("a lessthen or equal to b");
+                    }
+                        if(a==b) {
+                            System.out.println("a equal to equal to b");
+                        }
+                            if(a!=b){
+                                System.out.println("a not equal to b");
+                            }
+                        }
+                    }
+
+
+
+
