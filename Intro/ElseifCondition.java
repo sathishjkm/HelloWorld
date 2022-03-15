c7d82d61745a1be38e926b0fb681d7b5cbd7791b
sathis <latchulatchu02@gmail.com>
2022-03-15 15:43:29 +0530

    first commit

---
+++ Intro/ElseifCondition.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class ElseifCondition{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enterthe value a");
+        int a=scan.nextInt();
+        if(a<200) {
+            System.out.println("print good day");
+        }
+            else if(a<=200) {
+            System.out.println("print sunday");
+        }
+            else{
+                System.out.println("print bad day");
+            }
+        }
+            }
+
+
