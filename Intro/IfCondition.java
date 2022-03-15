0623aaadb97b822ff1880053cbb9204c611f9496
sathis <latchulatchu02@gmail.com>
2022-03-15 13:09:19 +0530

    first commit

---
+++ Intro/IfCondition.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class IfCondition{
+    public static void main(String[] args){
+        Scanner Scan=new Scanner(System.in);
+        System.out.println("enter the value a");
+        int a=Scan.nextInt();
+        if(a<=100){
+            System.out.println("a is inside 100");
+            }
+        else{
+            System.out.println("a is not inside 100");
+        }
+        }
+    }
