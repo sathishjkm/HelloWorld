a3380c90ef4cceb82470a776b48a142bedf3b4ff
sathis <latchulatchu02@gmail.com>
2022-03-15 16:08:54 +0530

    first commit

---
+++ Intro/OddEvenNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class OddEvenNumber{
+    public static void main(String[] args){
+    Scanner scan=new Scanner(System.in);
+    System.out.println("enter the x value");
+    int x=scan.nextInt();
+    if(x%2==0){
+        System.out.println("x is the even number");
+    }
+    else{
+        System.out.println("x is the odd number");
+    }
+}
+}
